1354e61acbd0104b732edacd621ed7fb4543094f
hara-PG <hara.job2021@gmail.com>
2022-05-31 14:14:56 +0900

    060_コールバック関数とthis

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,7 +1,24 @@
+window.name = "John";
+
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
-}
+  name: "Tom",
+  hello: function () {
+    console.log("Hello " + this.name);
+    a();
+
+    const person = {
+      name: "Tim",
+      hello: function () {
+        console.log("Hello " + this.name);
+        a();
+      },
+    };
+    person.hello();
+  },
+};
+
 person.hello();
+
+function a() {
+  console.log("hello " + this.name);
+}
