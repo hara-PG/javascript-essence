a84cd4334c19aff0c0cb3a4275699c4abad8fb9a
hara-PG <hara.job2021@gmail.com>
2022-05-31 14:03:04 +0900

    040_this

--- 050_関数とオブジェクト１/040_this/start/main.js
+++ 050_関数とオブジェクト１/040_this/start/main.js
@@ -1,6 +1,17 @@
 const person = {
-    name: 'Tom',
-    hello: function() {
+  name: "Tom",
+  hello: function () {
+    console.log("hello " + this.name);
+  },
+};
 
-    }
-}
+person.hello();
+
+const animal = {
+  name: "さくら",
+  type: function () {
+    console.log("柴犬の" + this.name);
+  },
+};
+
+animal.type();
