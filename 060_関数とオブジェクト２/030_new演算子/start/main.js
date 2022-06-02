4c441be1c2af38dea27258ecb59194980cf35bac
hara-PG <hara.job2021@gmail.com>
2022-06-02 14:53:05 +0900

    new_演算子

---
+++ 060_関数とオブジェクト２/030_new演算子/start/main.js
@@ -0,0 +1,12 @@
+function F(a, b) {
+  this.a = a;
+  this.b = b;
+  return {};
+}
+
+F.prototype.c = function () {};
+function newOpe(C, ...args) {
+  console.log(args);
+}
+const instance = newOpe(F, 1, 2);
+console.log(instance);
