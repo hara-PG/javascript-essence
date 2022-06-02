d977279a9a0e3a55f277df0b22c1599d10379825
hara-PG <hara.job2021@gmail.com>
2022-06-02 13:26:59 +0900

    100_アロー関数とthis

--- 050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
+++ 050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
@@ -1,9 +1,19 @@
-window.name = 'John';
+window.name = "John";
 
+const a = () => console.log("Bye " + this.name);
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
+  name: "Tom",
+  hello() {
+    console.log("Hello " + this.name);
+    a();
+  },
+};
+
+// person.hello();
+
+function b() {
+  const a = () => console.log("Bye " + this.name);
+  a();
 }
-person.hello();
+
+b();
