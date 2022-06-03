3eb96a33d82390ad6743b2e31a2fe15b83024d42
hara-PG <hara.job2021@gmail.com>
2022-06-03 22:41:23 +0900

    160_getter.setter.static

--- 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
+++ 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
@@ -1,4 +1,43 @@
 function Person1(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
+
+Object.defineProperty(Person1.prototype, "name", {
+  get: function () {
+    console.log("hello");
+    return this._name;
+  },
+  set: function (val) {
+    this._name = val;
+  },
+});
+
+const p1 = new Person1("Bob", 23);
+console.log(p1.name);
+
+p1.name = "Tom";
+console.log(p1.name);
+
+class Person2 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get name() {
+    console.log("hello");
+    return this._name;
+  }
+  set name(val) {
+    this._name = val;
+  }
+
+  static hello() {
+    console.log("hello");
+  }
+}
+
+const p2 = new Person2("Bob", 23);
+
+Person2.hello();
