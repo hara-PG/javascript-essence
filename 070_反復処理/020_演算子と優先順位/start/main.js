567fa50099aa82565b83a77d141e8fc82a2813c6
hara-PG <hara.job2021@gmail.com>
2022-06-06 12:12:59 +0900

    020_演算子と優先順位

---
+++ 070_反復処理/020_演算子と優先順位/start/main.js
@@ -0,0 +1,15 @@
+// let a = (1 + 2) * 3;
+// console.log(a);
+
+// let a = 0;
+// let b = ++a;
+// let b;
+// a = b = a + 1;
+// console.log(a, b);
+
+function fn() {
+  let a = 0;
+  return a++;
+}
+
+console.log(!(fn() * 5));
