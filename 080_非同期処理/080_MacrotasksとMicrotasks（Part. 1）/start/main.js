19e17dd41ea5353c48db20182eb447f6d65c6c67
hara-PG <hara.job2021@gmail.com>
2022-06-12 14:49:01 +0900

    080_macrotasksとmaictotasks

--- 080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
+++ 080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
@@ -1,12 +1,29 @@
-setTimeout(function task1() {
-  console.log("task1");
-});
-
 new Promise(function promise(resolve) {
   console.log("promise");
-  resolve();
-}).then(function job1() {
-  console.log("job1");
-});
+
+  setTimeout(function task1() {
+    console.log("task1");
+    resolve();
+  });
+})
+  .then(function job() {
+    console.log("job1");
+    setTimeout(function task1() {
+      console.log("task2");
+      const p = Promise.resolve();
+      p.then(function job4() {
+        console.log("job4");
+      });
+      queueMicrotask(function job4() {
+        console.log("job4");
+      });
+    });
+  })
+  .then(function job() {
+    console.log("job2");
+  })
+  .then(function job() {
+    console.log("job3");
+  });
 
 console.log("global end");
